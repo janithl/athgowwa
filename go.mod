f781bf7dc65b5e33ea25f9800d1407ab9728715a
Janith Leanage <janithleanage@gmail.com>
2019-10-04 04:36:50 +0530

    empty UI

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module athgowwa
 
 go 1.12
 
-require github.com/andlabs/ui v0.0.0-20180902183112-867a9e5a498d // indirect
+require github.com/andlabs/ui v0.0.0-20180902183112-867a9e5a498d
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"github.com/andlabs/ui"
+)
 
+func setupUI() {
+	mainwin := ui.NewWindow("Athgowwa", 360, 640, true)
+
+	mainwin.OnClosing(func(*ui.Window) bool {
+		ui.Quit()
+		return true
+	})
+
+	ui.OnShouldQuit(func() bool {
+		mainwin.Destroy()
+		return true
+	})
+
+	mainwin.Show()
+}
+
+func main() {
+	ui.Main(setupUI)
 }
